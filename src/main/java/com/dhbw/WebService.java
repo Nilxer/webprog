584c81f82084a96e4d09cf3f3ce365be91783518
Klaus Baumecker <klaus.baumecker@microfocus.com>
2020-02-18 17:00:44 +0100

    Plain WS

---
+++ src/main/java/com/dhbw/WebService.java
@@ -0,0 +1,42 @@
+package com.dhbw;
+
+import com.dhbw.ws.JSONWs;
+import com.dhbw.ws.PlainWs;
+import com.dhbw.ws.TinyWs;
+import io.dropwizard.Application;
+import io.dropwizard.assets.AssetsBundle;
+import io.dropwizard.setup.Bootstrap;
+import io.dropwizard.setup.Environment;
+
+public class WebService extends Application<WebServiceConfiguration>
+{
+  public static void main(String[] args) throws Exception
+  {
+    new WebService().run(args);
+  }
+
+  @Override
+  public String getName()
+  {
+    return "webService";
+  }
+
+  @Override
+  public void initialize(Bootstrap<WebServiceConfiguration> bootstrap)
+  {
+    bootstrap.addBundle(new AssetsBundle("/htmldocs", "/", "index.html", "static" ));
+  }
+
+  @Override
+  public void run(
+    WebServiceConfiguration config,
+    Environment environment) throws Exception
+  {
+    //environment.jersey().register(new JSONWs());
+    environment.jersey().register(new PlainWs());
+    //environment.jersey().register(new TinyWs());
+
+    final WSHealthCheck healthCheck = new WSHealthCheck();
+    environment.healthChecks().register("template", healthCheck);
+  }
+}
