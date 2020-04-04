5a48cbe2c6c1a50f92945edc7e7dfa186999a784
bowlen <philipp.bohlen@web.de>
2020-04-04 12:36:45 +0200

    Frontend erster Entwurf

---
+++ static/scripts/script.js
@@ -0,0 +1,11 @@
+function about() {
+  alert("This service was brought to you by students of Webprogrammierung at DHBW Stuttgart: David, Mika, Natalie and Philipp");
+}
+
+function contact() {
+    alert("Please contact wi18129@lehre.dhbw-stuttgart.de");
+  }
+
+function shorten(){
+    alert("This button looks nice, but doesn't work yet.")
+}
