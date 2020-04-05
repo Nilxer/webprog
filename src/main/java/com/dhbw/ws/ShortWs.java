4071c1a7dc9ee371eee118a743ba03778b13e022
bowlen <philipp.bohlen@web.de>
2020-04-05 13:10:46 +0200

    Fix: "static" directory Struktur funktionsfähig Feat: "Shorten" Button ruft API auf, jedoch noch nicht funktionsfähig ("HTTP method POST is not supported by this URL")

--- src/main/java/com/dhbw/ws/ShortWs.java
+++ src/main/java/com/dhbw/ws/ShortWs.java
@@ -12,6 +12,7 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
+
 import org.apache.commons.lang3.RandomStringUtils;
 
 import org.json.JSONObject;
