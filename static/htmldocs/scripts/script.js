59172e81f2e41b74f351924ded223752caa0c94c
bowlen <philipp.bohlen@web.de>
2020-04-08 09:50:00 +0200

    Updated Frontend Design

--- static/htmldocs/scripts/script.js
+++ static/htmldocs/scripts/script.js
@@ -1,15 +1,4 @@
-function about() {
-  alert("This service was brought to you by students of Webprogrammierung at DHBW Stuttgart: David, Mika, Natalie and Philipp");
-}
-
-function contact() {
-    alert("Please contact wi18129@lehre.dhbw-stuttgart.de");
-  }
-
-function shorten(){
-    alert("This button looks nice, but doesn't work yet.")
-}
-
-function godown(){
+function down(){
       window.location.href="#sec2";
 }
+
