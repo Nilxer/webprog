56365ac76df7ced6ac0af7fa4aa72571a8c91dd8
David Drexlin <david.drexlin@ibm.com>
2020-04-08 15:09:39 +0200

    feat: added js-capability to store and fetch data

--- static/htmldocs/scripts/script.js
+++ static/htmldocs/scripts/script.js
@@ -1,4 +1,25 @@
 function down(){
-      window.location.href="#sec2";
+      let input = '{'
+          +'"longUrl" : "'+document.getElementById("shortener-txt").value+'"'
+      +'}';
+
+      fetch('http://localhost:8080/api/short/', {
+            method: 'POST',
+            headers: {
+                  'Accept': 'application/json',
+                  'Content-Type': 'application/json',
+            },
+            body: input
+      }).then((data) => {
+          fetchURL()})
+          .catch(console.log)
 }
 
+
+function fetchURL(){
+      fetch('http://localhost:8080/api/short/')
+          .then(res => res.json())
+          .then((data) => {
+              console.log(data)})
+          .catch(console.log)
+}
